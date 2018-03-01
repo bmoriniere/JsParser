2aeb0c15e00cd54aa465311fe708f5bd51e7dfcd
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2018-03-01 19:09:12 +0100

    inputSample

--- 2018/modules/parser.js
+++ 2018/modules/parser.js
@@ -5,12 +5,42 @@ let lineCount = 0,
     parseError = false,
     parseMessage = '';
 
+const InFileSample = {
+    fileDesc : {
+        rows: 0,
+        columns: 0,
+        numberVehicules: 0,
+        numberRides: 0,
+        bonus: 0,
+        numberOfSteps: 0
+    },
+    rides: [
+        {
+            startPoint :{
+                row,
+                col
+            },
+            endPoint :{
+                row,
+                col
+            },
+            startStep: 0,
+            endStep: 0
+        }
+    ]
+}
+
+
+/**
+
+ */
+
 // Parsing function
 function parse(filePath){
     return new Promise(function(resolve, reject) {
-        lineCount = 0;        
+        lineCount = 0;
         let result = {};
-        
+
         var lineReader = require('readline').createInterface({
             input: require('fs').createReadStream(filePath)
         });
@@ -18,15 +48,15 @@ function parse(filePath){
         lineReader.on('line', (line)=>{
             if (lineCount === 0){
 
-            }else if (lineCount >= 2){                
+            }else if (lineCount >= 2){
                 // General case
-            }        
+            }
         });
-        
+
         lineReader.on('close', ()=>{
             if (parseError){
                 reject(parseMessage);
-            }else{                
+            }else{
                 resolve(result);
             }
         });
